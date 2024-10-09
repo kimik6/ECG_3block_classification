80340a0762ff1449b10b14434502b899580e91f2
kimik6 <kakimia595@gmail.com>
2024-10-09 16:30:20 +0330

    trying out correct code

--- ChapMan_CPSC_PTB_6_Classes/Train.py
+++ ChapMan_CPSC_PTB_6_Classes/Train.py
@@ -318,7 +318,7 @@ early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_Recall', mode='max', 
 
 for fold in range(NumOfFold):
     
-    checkpoint_filepath =f'/kaggle/working/ECG_3block_classification/Weights/Model_weights_{fold}_7Class_Chap_CPSC_PTB_PAC.hdf5'
+    checkpoint_filepath =f'/kaggle/working/ECG_3block_classification/ChapMan_CPSC_PTB_6_Classes/Weights/Model_weights_{fold}_7Class_Chap_CPSC_PTB_PAC.hdf5'
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
         save_weights_only=True,
