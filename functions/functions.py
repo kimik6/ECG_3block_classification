1e6f7bb6480b766d8ef3d0541958bb764763b02c
kimik6 <kakimia595@gmail.com>
2024-10-28 17:01:20 +0330

    changing the samples to 2500

--- functions/functions.py
+++ functions/functions.py
@@ -152,7 +152,7 @@ def generate_validation_data(ecg_filenames, y):
 
 def residual_network_1d(Dense_num,trainable,trainable_last_layer=True,trainableOnelast=True,Classifire=1,LR=10e-3):
     n_feature_maps = 64
-    input_shape = (None,12)
+    input_shape = (2500,12)
     input_layer = keras.layers.Input(input_shape)
 
     # BLOCK 1
